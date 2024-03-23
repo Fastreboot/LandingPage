eab73ca94e47fe5a17ae544cb5a0bbb6f268194e
akashkottil <kottilakash@gmail.com>
2024-03-23 11:44:09 +0530

    Updates

---
+++ assets/Home-DrM1vdOp.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/Hero-DKeA44gi.js","assets/index-BWFfRtmP.js","assets/index-CE9bJHVy.css","assets/Navbar-cErJ4HdW.js","assets/impact-Bu0uyi9j.js","assets/Banner-CVQg14K2.js","assets/Banner-aj7pw7i5.css","assets/Assessment-DB4qv3XV.js","assets/Testimonials-Bqw_beQk.js","assets/Footer-nmQPMayb.js","assets/FAQ-Dsq89ftj.js","assets/DemoContents-D5JE1Q7t.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{r as t,_ as o,j as _}from"./index-BWFfRtmP.js";const s=t.lazy(()=>o(()=>import("./Hero-DKeA44gi.js"),__vite__mapDeps([0,1,2,3]))),r=t.lazy(()=>o(()=>import("./impact-Bu0uyi9j.js"),__vite__mapDeps([4,1,2]))),e=t.lazy(()=>o(()=>import("./Banner-CVQg14K2.js"),__vite__mapDeps([5,1,2,6]))),i=t.lazy(()=>o(()=>import("./Assessment-DB4qv3XV.js"),__vite__mapDeps([7,1,2]))),a=t.lazy(()=>o(()=>import("./Testimonials-Bqw_beQk.js"),__vite__mapDeps([8,1,2]))),n=t.lazy(()=>o(()=>import("./Footer-nmQPMayb.js"),__vite__mapDeps([9,1,2]))),E=t.lazy(()=>o(()=>import("./FAQ-Dsq89ftj.js"),__vite__mapDeps([10,1,2]))),m=t.lazy(()=>o(()=>import("./DemoContents-D5JE1Q7t.js"),__vite__mapDeps([11,1,2]))),x=()=>_.jsxs("div",{children:[_.jsx(s,{}),_.jsx(r,{}),_.jsx(e,{}),_.jsx(i,{}),_.jsx(a,{}),_.jsx(m,{}),_.jsx(E,{}),_.jsx(n,{})]});export{x as default};
