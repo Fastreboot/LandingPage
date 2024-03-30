0bfa01fdd59af1424db682d9dc18118c84942379
akashkottil <kottilakash@gmail.com>
2024-03-30 21:17:05 +0530

    Added options in assessment page

--- src/Pages/QNA/QNA.jsx
+++ src/Pages/QNA/QNA.jsx
@@ -4,23 +4,103 @@ import AssessmentStepper from '../../Components/AssessmentStepper/AssessmentStep
 const AssessmentSteps = [
     {
         name: " question 1",
-        Component: () => <div className=' text-center '>first question</div>,
+        Component: () => <div className=' text-center '><h1>first question</h1>
+        <div className='options flex justify-center items-center gap-8'>
+                            <label>
+                                <input type="checkbox" name='questions' />
+                                
+                                Option A
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions'/>
+                                Option B
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions' />
+                                Option C
+                            </label>
+                        </div>
+        </div>,
     },
     {
         name: " question 2",
-        Component: () => <div className=' text-center '>second question</div>,
+        Component: () => <div className=' text-center '><h1>Second question</h1>
+        <div className='options flex justify-center items-center gap-8'>
+                            <label>
+                                <input type="checkbox" name='questions' />
+                                
+                                Option A
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions'/>
+                                Option B
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions' />
+                                Option C
+                            </label>
+                        </div>
+        </div>,
     },
     {
         name: " question 3",
-        Component: () => <div className=' text-center '>third question</div>,
+        Component: () => <div className=' text-center '><h1>third question</h1>
+        <div className='options flex justify-center items-center gap-8'>
+                            <label>
+                                <input type="checkbox" name='questions' />
+                                
+                                Option A
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions'/>
+                                Option B
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions' />
+                                Option C
+                            </label>
+                        </div>
+        </div>,
     },
     {
         name: " question 4",
-        Component: () => <div className=' text-center '>fourth question</div>,
+        Component: () => <div className=' text-center '><h1>fourth question</h1>
+        <div className='options flex justify-center items-center gap-8'>
+                            <label>
+                                <input type="checkbox" name='questions' />
+                                
+                                Option A
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions'/>
+                                Option B
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions' />
+                                Option C
+                            </label>
+                        </div>
+        </div>,
     },
     {
         name: " question 5",
-        Component: () => <div className=' text-center '>fifth question</div>,
+        Component: () => <div className=' text-center '><h1>fifth question</h1>
+        <div className='options flex justify-center items-center gap-8'>
+                            <label>
+                                <input type='checkbox' name='questions' />
+                                
+                                Option A
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions'/>
+                                Option B
+                            </label>
+                            <label>
+                            <input type="checkbox" name='questions' />
+                                Option C
+                            </label>
+                        </div>
+        </div>,
     },
 ]
 
