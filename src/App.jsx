3931adedfd6c064c2ed4f39f968745d558a2f6fe
Imad Ibrahim <imadibrahim164@gmail.com>
2024-03-31 13:28:34 +0530

    removed lazy loading from app.js

--- src/App.jsx
+++ src/App.jsx
@@ -36,12 +36,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
-const Program = lazy(() => import('./Pages/Program/Program'));
-const Login = lazy(() => import('./Pages/Login/Login'));
-const Home = lazy(() => import('./Pages/Home/Home'));
-const About = lazy(() => import('./Pages/About/About'));
-const Contact = lazy(() => import('./Pages/Contact/Contact'));
-const QNA = lazy(()=>import('./Pages/QNA/QNA'))
+// const Program = lazy(() => import('./Pages/Program/Program'));
+// const Login = lazy(() => import('./Pages/Login/Login'));
+// const Home = lazy(() => import('./Pages/Home/Home'));
+// const About = lazy(() => import('./Pages/About/About'));
+// const Contact = lazy(() => import('./Pages/Contact/Contact'));
+// const QNA = lazy(()=>import('./Pages/QNA/QNA'))
+
+import Program from './Pages/Program/Program';
+import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import QNA from './Pages/QNA/QNA';
 
 const App = () => {
   return (
